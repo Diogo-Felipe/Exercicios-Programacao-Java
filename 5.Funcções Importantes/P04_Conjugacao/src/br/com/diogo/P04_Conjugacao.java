44adb7e7ec2a7d035ba8a4259092b1164f952208
Diogo Felipe <diogomem@outlook.com>
2019-09-23 09:10:07 -0300

    Correção de exercício

--- 5.Funcções Importantes/P04_Conjugacao/src/br/com/diogo/P04_Conjugacao.java
+++ 5.Funcções Importantes/P04_Conjugacao/src/br/com/diogo/P04_Conjugacao.java
@@ -15,11 +15,13 @@ public class P04_Conjugacao {
 		duasUltimasLetras = verbo.substring(verbo.length() - 2);
 		
 		if (duasUltimasLetras.equals("ar")) {
-			
+			System.out.println("Verbo na primeira conjugação");
 		} else if ((duasUltimasLetras.equals("er")) || (duasUltimasLetras.equals("or"))) {
-			
+			System.out.println("Verbo na segunda conjugação");
 		} else if (duasUltimasLetras.equals("ir")) {
-			
+			System.out.println("Verbo na terceira conjugação");
+		} else {
+			System.out.println("Não é um verbo");
 		}
 		
 	}
