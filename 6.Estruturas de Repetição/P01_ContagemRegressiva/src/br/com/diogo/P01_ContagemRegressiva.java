a68e41729282eb7b53871c49d5977b4adeabe6cd
Diogo Felipe <diogomem@outlook.com>
2019-09-23 15:39:38 -0300

    Adição do primeiro exercício de estruturas de repetição

---
+++ 6.Estruturas de Repetição/P01_ContagemRegressiva/src/br/com/diogo/P01_ContagemRegressiva.java
@@ -0,0 +1,32 @@
+package br.com.diogo;
+
+public class P01_ContagemRegressiva {
+
+	public static void main(String[] args) {
+		int n = 10;
+		
+		System.out.println("do-while");
+		do {
+			System.out.println(n);
+			n--;
+		}while(n > 0);
+		
+		n = 10;
+		
+		System.out.println("");
+		System.out.println("while");
+		while(n > 0) {
+			System.out.println(n);
+			n--;
+		}
+		
+		System.out.println("");
+		System.out.println("for");
+		for(int i = 10; i > 0 ;i--) {
+			System.out.println(i);
+		}
+		
+		
+	}
+
+}
