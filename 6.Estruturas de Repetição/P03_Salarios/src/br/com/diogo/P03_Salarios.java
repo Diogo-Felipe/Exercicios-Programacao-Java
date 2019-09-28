131b6d25bf258b7170ab8685dacee76a0e42d983
Diogo Felipe <diogomem@outlook.com>
2019-09-28 14:03:36 -0300

    Adicinado exercício 3 de estruturas de repetição

---
+++ 6.Estruturas de Repetição/P03_Salarios/src/br/com/diogo/P03_Salarios.java
@@ -0,0 +1,28 @@
+package br.com.diogo;
+
+import java.util.Scanner;
+
+public class P03_Salarios {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		double salario = 0;
+		double somaSalarios = 0;
+		double quinzePorcentoSalario = 0;
+		
+		do {
+			System.out.println("Digite o salario: ");
+			salario = sc.nextDouble();
+			
+			somaSalarios = somaSalarios + salario;
+		} while(salario > 0);
+		
+		quinzePorcentoSalario = somaSalarios * 0.15;
+		
+		System.out.println("Somatório dos salários é: " + somaSalarios);
+		System.out.println("Quinze porcento desse somatório é: " + quinzePorcentoSalario);
+		
+	}
+
+}
