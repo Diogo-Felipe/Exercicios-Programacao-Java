48db0b2f0783f18f5fe90aadfa8b6a0fda9ceb8b
Diogo Felipe <dlcfelipp@gmail.com>
2019-11-20 10:13:11 -0300

    Exercício 2 a

---
+++ 10.Funções/P02_FuncImpressaoA/src/br/com/diogo/P02_FuncImpressaoA.java
@@ -0,0 +1,13 @@
+package br.com.diogo;
+
+public class P02_FuncImpressaoA {
+
+	public static void main(String[] args) {
+		System.out.println("************");
+		System.out.println("PROGRAMAÇÃO");
+		System.out.println("************");
+		System.out.println("DE SISTEMAS");
+
+	}
+
+}
