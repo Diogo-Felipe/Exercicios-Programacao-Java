a5d9233a12425e2f0d67250572377ea34f8b84cb
Diogo Felipe <dlcfelipp@gmail.com>
2019-11-20 10:11:19 -0300

    Exercício 1

---
+++ 10.Funções/P01_ FuncSoma/src/br/com/diogo/P01_FuncSoma.java
@@ -0,0 +1,18 @@
+package br.com.diogo;
+
+public class P01_FuncSoma {
+
+	public static void main(String[] args) {
+		int a = 4;
+		int b = 4;
+		
+		System.out.print(soma(a, b));
+	}
+	
+	static int soma (int n1, int n2){
+		int r; 
+		r = n1 + n2;
+		return r;
+	}
+	
+}
