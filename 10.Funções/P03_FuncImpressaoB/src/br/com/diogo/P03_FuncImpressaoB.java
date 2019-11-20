0f9bcf1793c0ab016a6e52152ac86074a778d63d
Diogo Felipe <dlcfelipp@gmail.com>
2019-11-20 10:13:49 -0300

    Exercício 2 b

---
+++ 10.Funções/P03_FuncImpressaoB/src/br/com/diogo/P03_FuncImpressaoB.java
@@ -0,0 +1,17 @@
+package br.com.diogo;
+
+public class P03_FuncImpressaoB {
+
+	public static void main(String[] args) {
+		separador();
+		System.out.println("PROGRAMAÇÃO");
+		separador();
+		System.out.println("DE SISTEMAS");
+
+	}
+	
+	static void separador() {
+		System.out.println("************");
+	}
+
+}
